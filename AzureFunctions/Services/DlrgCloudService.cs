7a5eebf4400aaf2a48eb3c0863f80a6f5479046b
Alexander Beck <alexander.beck@microsoft.com>
2024-01-14 10:01:41 +0100

    Add dynamic directory creation in DlrgCloudService

--- AzureFunctions/Services/DlrgCloudService.cs
+++ AzureFunctions/Services/DlrgCloudService.cs
@@ -20,6 +20,26 @@ public class DlrgCloudService
         _httpClient.DefaultRequestHeaders.Authorization = authValue;
     }
 
+    
+
+    //Create a method that sends a MKCOL request to the DLRG Cloud
+    public Task<string> CreateDirectory(string directoryDir)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = new HttpMethod("MKCOL"),
+            RequestUri = new Uri("https://www.dlrg.cloud/remote.php/dav/files/albe/1904000-Leitung%20Einsatz/10-FB_Bootswesen/Test/Sachsen/")
+        };
+        
+        return _httpClient.SendAsync(request).ContinueWith(responseTask =>
+        {
+            var response = responseTask.Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStringAsync();
+        }).Unwrap();
+    }
+
+
     //URL https://www.dlrg.cloud/remote.php/dav/files/albe/1904000-Leitung%20Einsatz/10-FB_Bootswesen/Test/
     public async Task<string> UploadBlobToDlrgCloudAsync(string endpoint, byte[] content)
     {
